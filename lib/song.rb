3e647d36cf853d6bfc209b346368566cbfa3d2f4
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-08-25 21:07:52 +0300

    commit tree 2435c9eb836c9680d622996190744c87df992a29

--- lib/song.rb
+++ lib/song.rb
@@ -59,17 +59,17 @@ class Song
       self.new_from_db(row)
     end
   end
-  def self.find_by_name(name)
+  def self.find_by_name
     sql = <<-SQL
       SELECT *
       FROM songs
       WHERE name = ?
       LIMIT 1
     SQL
-
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql,name).map do |row|
       self.new_from_db(row)
     end.first
+    
   end
 
 end
